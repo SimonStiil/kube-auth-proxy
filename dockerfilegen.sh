9fdcace93cf55d7e0ece2c0ee06d2a53fa4558d5
simon <130985998+SimonStiil@users.noreply.github.com>
2024-04-27 17:49:25 +0200

    adding arm build process

--- dockerfilegen.sh
+++ dockerfilegen.sh
@@ -1,11 +1,9 @@
 source package.env
 
-echo -n "FROM alpine:latest as certs
-RUN apk --update add ca-certificates 
-FROM ${PACKAGE_CONTAINER_BASE}
+echo -n "FROM ${PACKAGE_CONTAINER_BASE}
 WORKDIR ${PACKAGE_CONTAINER_WORKDIR}
-COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-COPY ${PACKAGE_CONTAINER_APPLICATION} ${PACKAGE_CONTAINER_DESTINATION}
+COPY ca-certificates.crt /etc/ssl/certs/
+COPY ${PACKAGE_CONTAINER_APPLICATION}-${1} ${PACKAGE_CONTAINER_DESTINATION}
 LABEL org.opencontainers.image.source ${PACKAGE_CONTAINER_SOURCE}
 LABEL org.opencontainers.image.licenses ${PACKAGE_CONTAINER_LICENSE}
 ENTRYPOINT [\"${PACKAGE_CONTAINER_APPLICATION}\"]" >Dockerfile
